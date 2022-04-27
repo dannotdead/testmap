c9120a7aab57bd26251fc1db5cf55008d54b1d1b
dannotdead <daniil.koka2000@gmail.com>
2022-04-27 18:39:54 +0700

    create main page template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,13 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { AppComponent }   from './app.component';
+import { MenuComponent } from './modules/menu/menu.component';
+import { MapComponent } from './modules/map/map.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatButtonModule } from '@angular/material/button';
 @NgModule({
-    imports:      [ BrowserModule, FormsModule ],
-    declarations: [ AppComponent ],
+    imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule, MatButtonModule ],
+    declarations: [ AppComponent, MenuComponent, MapComponent ],
     bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
