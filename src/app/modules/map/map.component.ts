5f29b943ad873e1f86d48826f8e1fa260489e44a
dannotdead <daniil.koka2000@gmail.com>
2022-04-28 11:32:34 +0700

    added drawing points on click on the map area

--- src/app/modules/map/map.component.ts
+++ src/app/modules/map/map.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import Map from 'ol/Map';
 import View from 'ol/View';
-import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
+import VectorSource from 'ol/source/Vector';
+import {Circle as CircleStyle, Fill, Style} from 'ol/style';
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
+import {Draw, Modify, Snap} from 'ol/interaction';
      
 @Component({
     selector: 'map',
@@ -12,7 +15,31 @@ import OSM from 'ol/source/OSM';
 
 export class MapComponent implements OnInit { 
     name= '';
+
     map: Map;
+
+    source = new VectorSource();
+
+    style = new Style({
+        image: new CircleStyle({
+            radius: 7,
+            fill: new Fill({
+                color: '#ffcc33',
+            }),
+        }),
+    });
+
+    modify = new Modify({
+        source: this.source,
+        style: this.style
+    });
+
+    draw = new Draw({
+        source: this.source,
+        type: 'Point',
+    });
+
+    snap = new Snap({source: this.source});
 
     ngOnInit(): void {
         this.map = new Map({
@@ -22,10 +49,20 @@ export class MapComponent implements OnInit {
         }),
         layers: [
             new TileLayer({
-            source: new OSM(),
+                source: new OSM(),
             }),
+            new VectorLayer({
+                source: this.source,
+                style: this.style
+            })
         ],
         target: 'ol-map'
         });
+    } 
+
+    addInteractions(): void {
+        this.map.addInteraction(this.modify);
+        this.map.addInteraction(this.draw);
+        this.map.addInteraction(this.snap);
     }
 }
